a37037762a8e33b6f39e1f3487485cf974ad8712
artnoi <pphansuriyanon@gmail.com>
2021-09-12 00:07:30 +0700

    Updated build script

--- frontend/build_sed_copy.sh
+++ frontend/build_sed_copy.sh
@@ -5,4 +5,4 @@ dest="/srv/http/doggovac_frontend"
 yarn run build\
 && sed -i 's/Web site created using create-react-app/Full Stack Pet Vaccination Management/' "$target"\
 && sed -i 's/React App/DoggoVac/' "$target"\
-&& sudo rsync -avP --delete build "$dest";
+&& sudo rsync -avP --delete build/* "$dest";
